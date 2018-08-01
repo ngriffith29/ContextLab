b522731c7d3989b6cad0834ca6bc7edf535722ff
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-01 12:59:09 -0400

    This is the lab on a way that made more sense to me. Ask about the loop they want

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
 //Variables 
+var num2 = 0
 var count = 0
+var friendName = ["Jenna", "Clay", "Morgan", "Rachel", "Paul"]
+var locations = ["The backyard", "Pantry", "Basement", "Living Room", "Kitchen", "Master Bathroom", "Pool", "Front Porch", "Back Porch", "Driveway"]
+var weapons = ["Ar-15", "Russian Hackers", "Garrote Wire", "Pen Dagger", "Lapel Spike", "Concealment Key",
+"Shotgun", "Suicide Glasses", "Bulgarian Umbrella", "Pistol", "Machine Gun", "Bowling Ball", "Butcher Knife", 
+"Chain", "Brass Knuckles","Mowing Blades", "Hatchet","Baseball Bat", "caddle proder", "weed wacker", "Chain saw"]
+
 //Function Declarations\
+
+
 //Function below creates 100 h3s
     function create100(num) {       
         for(var i = 0; i<num; i++){
@@ -9,14 +18,31 @@ var count = 0
            
         }        
     }   
-    //function below creates 100 h3s and counts them up by 1
+
+    //function below creates 100 h3s and counts them up by ++
     function createH3 (){
         var h3 = $('<h3></h3>').text(`Accusation ${count += 1}`)
             $("body").append(h3)
     }
-//Below are all my function calls
-create100(100)  
+    //Function Creates the indexes for the 3 arrays
+   function indexes (){ for(let i = 0; i<num2; i++){
 
+
+
+    }
+   }
+//Below are all my function calls
+create100(100)
+
+//Event listner
+
+$('h3').on("click", function (){
+   
+   alert(`I accuse ${friendName[Math.floor(Math.random() * 5)]} with the
+   ${weapons[Math.floor(Math.random() * 20)]} in the ${locations[Math.floor(Math.random() * 10)]}!`)
+
+
+})
 
 
 })
